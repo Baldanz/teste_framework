f4fc7eb0ec82b12f2786f1ab1e494acef263ebae
Leonardo Baldan Azevedo <leonardo.baldan@gmail.com>
2021-12-13 10:42:08 -0300

    criação do projeto

---
+++ TesteFramework.Console/Calculo.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace TesteFramework
+{
+    public class Calculo
+    {
+        #region métodos
+
+        private StringBuilder CalcularPrimos(List<int> Divisores)
+        {
+            // declaração de variáveis
+            StringBuilder SequencialPrimos = new StringBuilder()
+                .AppendLine("")
+                .AppendLine("Divisores Primos");
+            bool ehPrimo = true;
+
+            // laço para identificar os números divisores
+            foreach (int num in Divisores)
+            {
+                // contador para buscar os divisores
+                for (int i = 0; i <= num; i++)
+                {
+                    if ((i == 0) || (i == num) || (i == 1))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        // laço para encontrar os divisores primos dos divisores do número informado
+                        for (int j = i; j < num; j++)
+                        {
+                            if (num % j == 0)
+                            {
+                                ehPrimo = false;
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // identificação dos números primos dos divisores do número informado
+                if (ehPrimo)
+                    SequencialPrimos.AppendLine(num.ToString());
+            }
+
+            return SequencialPrimos;
+        }
+
+
+        /// <summary>
+        /// calcula os dividisores do número informado
+        /// </summary>
+        /// <param name="Numero">número informado para efetivar o cálculo dos divisores</param>
+        /// <returns>retorna o sequencial de divisores do número informado</returns>
+        public StringBuilder CalcularDivisores(int Numero)
+        {
+            // bloco de variáveis
+            StringBuilder SequencialDivisores = new StringBuilder()
+                .AppendLine("")
+                .AppendLine("Números Divisores");
+            List<int> divisores = new List<int>();
+
+            // neste ponto, vamos encontrar os divisores do número informado
+            for (int i = 1; i <= Numero; i++)
+            {
+                // implementa o cálculo que identifica se o número é divisor dele, sendo o resto da divisão igual a zero
+                int divisor = (Numero % i);
+
+                // se o resto da divisão for zero, então encontramos o divisor
+                if (divisor == 0)
+                {
+                    // monta a sequencia
+                    divisores.Add(i);
+                    SequencialDivisores.AppendLine(string.Join(',', i.ToString()));
+                }
+            }
+            SequencialDivisores.AppendLine(CalcularPrimos(divisores).ToString());
+            return SequencialDivisores;
+        }
+
+        #endregion
+    }
+}
---
+++ TesteFramework.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TesteFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número");
+            string Numero = Console.ReadLine();
+
+            // condição para efetuar os cálculos dos divisores e números primos
+            if (String.IsNullOrEmpty(Numero.ToString()) == Numero.ToString().All(char.IsDigit) == false)
+            {
+                StringBuilder Sequencial = new Calculo().CalcularDivisores(int.Parse(Numero));
+                Console.WriteLine(Sequencial.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível efetivar o cálculo. Refaça a operação!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
